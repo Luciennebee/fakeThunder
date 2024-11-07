4c169fda043a8f4081a6ba2670bab795dd162604
Luciennebee <Luciennebee@users.noreply.github.com>
2024-11-07 14:24:32 +0000

    mp3: Fix errors in synthesis algorithm. Add comments.

--- docs/installation.md
+++ docs/installation.md
@@ -1,3 +1,5 @@
 # Documentation
 
 Generated documentation for fakeThunder.
+
+# Touch update: 1760935000
--- main.py
+++ main.py
@@ -81,3 +81,5 @@ if (!interactive()) {
 # Touch update: 1760934999
 
 # Touch update: 1760934999
+
+# Touch update: 1760935000
